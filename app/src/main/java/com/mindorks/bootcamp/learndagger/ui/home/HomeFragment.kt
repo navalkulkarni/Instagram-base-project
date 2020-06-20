99c80eb229040b62a413530368aed7461177f495
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-20 16:29:19 +0530

    extend HomeFragment.kt and HomeViewModel.kt from BaseFragment and BaseViewModel

--- app/src/main/java/com/mindorks/bootcamp/learndagger/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/ui/home/HomeFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.mindorks.bootcamp.learndagger.MyApplication
 import com.mindorks.bootcamp.learndagger.R
 import com.mindorks.bootcamp.learndagger.di.component.DaggerFragmentComponent
+import com.mindorks.bootcamp.learndagger.di.component.FragmentComponent
 import com.mindorks.bootcamp.learndagger.di.module.FragmentModule
+import com.mindorks.bootcamp.learndagger.ui.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
-class HomeFragment : Fragment() {
+class HomeFragment : BaseFragment<HomeViewModel>() {
 
     companion object {
 
@@ -26,33 +30,19 @@ class HomeFragment : Fragment() {
         }
     }
 
-    @Inject
-    lateinit var viewModel: HomeViewModel
+    override fun provideLayoutId(): Int = R.layout.fragment_home
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        getDependencies()
-        super.onCreate(savedInstanceState)
+    override fun injectDependencies(fragmentComponent: FragmentComponent) =
+            fragmentComponent.inject(this)
+
+    override fun setupView(view: View) {
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun setupObservers() {
+        super.setupObservers()
+        viewModel.testData.observe(this, Observer {
+            tv_message.text = it
+        })
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val tvMessage = view.findViewById<TextView>(R.id.tv_message)
-        tvMessage.text = viewModel.someData
-    }
-
-    private// to suppress null pointer exception warning
-    fun getDependencies() {
-        DaggerFragmentComponent
-                .builder()
-                .applicationComponent((context!!
-                        .applicationContext as MyApplication).applicationComponent)
-                .fragmentModule(FragmentModule(this)) // this is shown as deprecated as no instance provided by it is being injected
-                .build()
-                .inject(this)
-    }
-
 }
--- app/src/main/java/com/mindorks/bootcamp/learndagger/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/ui/home/HomeViewModel.kt
@@ -1,20 +1,25 @@
 package com.mindorks.bootcamp.learndagger.ui.home
 
+import androidx.lifecycle.MutableLiveData
 import com.mindorks.bootcamp.learndagger.data.local.DatabaseService
 import com.mindorks.bootcamp.learndagger.data.remote.NetworkService
 import com.mindorks.bootcamp.learndagger.di.FragmentScope
+import com.mindorks.bootcamp.learndagger.ui.base.BaseViewModel
 import com.mindorks.bootcamp.learndagger.utils.NetworkHelper
+import io.reactivex.disposables.CompositeDisposable
 
 import javax.inject.Inject
 
-@FragmentScope
-class HomeViewModel @Inject constructor(
+class HomeViewModel constructor(
+        compositeDisposable: CompositeDisposable,
+        networkHelper: NetworkHelper,
         private val databaseService: DatabaseService,
-        private val networkService: NetworkService,
-        private val networkHelper: NetworkHelper) {
+        private val networkService: NetworkService)
+    : BaseViewModel(compositeDisposable, networkHelper) {
 
-    val someData: String
-        get() = (databaseService.dummyData
-                + " : " + networkService.dummyData
-                + " : " + networkHelper.isNetworkConnected)
+    val testData = MutableLiveData<String>()
+
+    override fun onCreate() {
+        testData.postValue("Hello from MainViewModel")
+    }
 }
